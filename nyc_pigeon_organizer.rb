86541ffc3375bb117cc834a7e47769a4a1fd114b
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-06-01 03:24:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  end_result = data.each_with_object({}) do |(key, value), end_result|
+    #binding.pry
+    value.each do |inner_key, names|
+      #binding.pry
+      names.each do |name|
+        if !end_result[name]
+          end_result[name] = {}
+        end
+        if !end_result[name][key]
+          !end_result[name][key] = []
+        end
+        end_result[name][key].push(inner_key.to_s)
+        #binding.pry
+      end
+    end
+    end_result
+  end 
+ # binding.pry
 end
